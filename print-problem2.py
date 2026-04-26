f3a8660e1a4d6ff47370fad4ca4d139daf06dce0
Shounak Hazra <shounakhazra44@gmail.com>
2026-04-26 19:59:44 +0000

    3rd

---
+++ print-problem2.py
@@ -0,0 +1,8 @@
+name = "Shounak"
+city = "Kolkata"
+age = 20
+fav_number = 7
+print(f"My name is {name}")
+print(f"I live in {city}")
+print(f"I am {age} years old")
+print(f"My favourite number is {fav_number}")
