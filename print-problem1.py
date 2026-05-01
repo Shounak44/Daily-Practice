cf829c06400cfd0d9650e1e9c4502754aab92ba7
Shounak Hazra <shounakhazra44@gmail.com>
2026-05-01 15:05:58 +0000

    new

--- print-problem1.py
+++ print-problem1.py
@@ -3,6 +3,6 @@ city = "Kolkata"
 age = 20
 fav_number = 7
 print("my name is ",name)
-print("I live in ",city)
-print("I am years old ",age)
+print("I live in",city)
+print("I am",age, "years old ")
 print("my favorite number is ",fav_number)
