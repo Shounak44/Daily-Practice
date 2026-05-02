0fd942ec9340e3bfba6aaac57656a542bece7a1a
Shounak44 <shounakhazra44@gmail.com>
2026-05-02 17:56:03 +0530

    new

---
+++ poor-or-rich.py
@@ -0,0 +1,13 @@
+paisa = int(input("kitna paisa hai tere pass ? = "))
+if paisa >= 500 :
+    print("bhai tu to ameer hai")
+elif paisa >= 300 :
+    print("bhai tu to thoda ameer hai")
+elif paisa >= 150 :
+    print("bhai tu to thoda garib hai")
+elif paisa >= 50 :
+    print("bhai tu to garib hai")
+elif paisa >+ 10 :
+    print("bhai tu to bahut garib hai")
+else :
+    print("bhai tu to bahut hi garib hai")
